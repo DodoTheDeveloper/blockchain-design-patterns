b28d865de52077058721a96f7bc48b27a8d7e942
Dodo <dodothedeveloper@gmail.com>
2019-12-08 14:00:54 +0100

    #2

--- Bank.sol
+++ Bank.sol
@@ -3,18 +3,17 @@ pragma solidity >=0.4.22 <0.6.0;
 contract Bank {
     
     mapping(address => uint256) public savings;
-    
     constructor () public {}
     
     /*
-     * Pay into your bank account.
+     * Pay into a bank account.
      */
     function addSaving(address _to) payable public {
         savings[_to] += msg.value;
     }
     
     /*
-     * Take money out of the bank.
+     * Take money out of my bank account.
      */
     function takeSaving(uint256 _amount) public {
         require(_amount > 0);
@@ -24,12 +23,12 @@ contract Bank {
     }
     
     /*
-     * Send money to another bank account.
+     * Send ether from my bank account to another one.
      */
-    function sendSaving(uint256 _amount, address _from, address _to) public {
-        require(savings[_from] >= _amount);
+    function sendSaving(uint256 _amount, address _to) public {
+        require(savings[msg.sender] >= _amount);
         
-        savings[_from] -= _amount;
+        savings[msg.sender] -= _amount;
         savings[_to] += _amount;
     }
     
